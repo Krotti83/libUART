b97b5ae02e54de1991f4338919765fd2ad7f1727
Johannes Krottmayer <krotti83@proton.me>
2025-07-15 02:01:25 +0200

    src/files.mk: add Windows support
    - Add initial Windows DLL build support.
    
    FILE(s):
    M  src/files.mk
    
    Signed-off-by: Johannes Krottmayer <krotti83@proton.me>

--- src/files.mk
+++ src/files.mk
@@ -20,6 +20,7 @@
 
 LIBUART_DIR				= ./src
 
+ifneq ($(CONFIG_BUILD_OS),win32)
 LIBUART_SCSRC				+= $(BUILD_DIR)/static/posix_error.c
 LIBUART_SCSRC				+= $(BUILD_DIR)/static/posix_system.c
 LIBUART_SCSRC				+= $(BUILD_DIR)/static/uart.c
@@ -29,3 +30,11 @@ LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/posix_error.c
 LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/posix_system.c
 LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/uart.c
 LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/util.c
+else
+LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/win32_error.c
+LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/win32_system.c
+LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/uart.c
+LIBUART_DCSRC				+= $(BUILD_DIR)/dynamic/util.c
+
+LIBUART_DRSRC				+= $(BUILD_DIR)/dynamic/win32_resource.rc
+endif
