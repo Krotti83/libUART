a17527aefc2567e51b437efd5ec858f3a0baab80
Johannes Krottmayer <krotti83@proton.me>
2025-07-16 19:19:21 +0200

    src: add POSIX threading functions (stub)
    - Add initial POSIX threading functions.
    
    NOTE: This is currently a stub.
    
    FILE(s):
    A  src/posix_thread.c
    
    Signed-off-by: Johannes Krottmayer <krotti83@proton.me>

---
+++ src/posix_thread.c
@@ -0,0 +1,192 @@
+/**
+ *
+ * libUART
+ *
+ * Easy to use library for accessing the UART
+ *
+ * Copyright (c) 2025 Johannes Krottmayer <krotti83@proton.me>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#include <pthread.h>
+#include <unistd.h>
+
+#include "system.h"
+
+#include <UART.h>
+
+int _uart_thread_init(uart_t *uart)
+{
+    int ret;
+
+    ret = pthread_mutex_init(&uart->rx_mutex, NULL);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_init(&uart->tx_mutex, NULL);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_init(&uart->rx_lock, NULL);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_init(&uart->tx_lock, NULL);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    return UART_ESUCCESS;
+}
+
+void *worker_thread_rx(void *p)
+{
+    int run = 1;
+    uart_t *uart = (uart_t *) p;
+
+    while (run) {
+        usleep(100);
+        pthread_mutex_lock(&uart->rx_lock);
+
+        // TODO
+
+        pthread_mutex_unlock(&uart->rx_lock);
+
+        pthread_mutex_lock(&uart->rx_mutex);
+
+        if (!uart->rx_thread_run) {
+            run = 0;
+        }
+
+        pthread_mutex_unlock(&uart->rx_mutex);
+    }
+
+    return UART_ESUCCESS;
+}
+
+void *worker_thread_tx(void *p)
+{
+    int run = 1;
+    uart_t *uart = (uart_t *) p;
+
+    while (run) {
+        usleep(100);
+        pthread_mutex_lock(&uart->tx_lock);
+
+        // TODO
+
+        pthread_mutex_unlock(&uart->tx_lock);
+
+        pthread_mutex_lock(&uart->tx_mutex);
+
+        if (!uart->tx_thread_run) {
+            run = 0;
+        }
+
+        pthread_mutex_unlock(&uart->tx_mutex);
+    }
+
+    return NULL;
+}
+
+int _uart_thread_start(uart_t *uart)
+{
+    int ret;
+
+    uart->rx_thread_run = 1;
+    uart->tx_thread_run = 1;
+    ret = pthread_create(&uart->rx_thread, NULL, worker_thread_rx, (void *) uart);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_create(&uart->tx_thread, NULL, worker_thread_tx, (void *) uart);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    return UART_ESUCCESS;
+}
+
+int _uart_thread_stop(uart_t *uart)
+{
+    int ret;
+
+    pthread_mutex_lock(&uart->rx_mutex);
+    uart->rx_thread_run = 0;
+    pthread_mutex_unlock(&uart->rx_mutex);
+
+    pthread_mutex_lock(&uart->tx_mutex);
+    uart->tx_thread_run = 0;
+    pthread_mutex_unlock(&uart->tx_mutex);
+
+    pthread_join(uart->rx_thread, NULL);
+    pthread_join(uart->tx_thread, NULL);
+
+    ret = pthread_mutex_destroy(&uart->rx_mutex);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_destroy(&uart->tx_mutex);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_destroy(&uart->rx_lock);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    ret = pthread_mutex_destroy(&uart->tx_lock);
+
+    if (ret != 0) {
+        return UART_ESYSTEM;
+    }
+
+    return UART_ESUCCESS;
+}
+
+void _uart_thread_lock_rx(uart_t *uart)
+{
+    pthread_mutex_lock(&uart->rx_lock);
+}
+
+void _uart_thread_lock_tx(uart_t *uart)
+{
+    pthread_mutex_lock(&uart->tx_lock);
+}
+
+void _uart_thread_unlock_rx(uart_t *uart)
+{
+    pthread_mutex_unlock(&uart->rx_lock);
+}
+
+void _uart_thread_unlock_tx(uart_t *uart)
+{
+    pthread_mutex_unlock(&uart->tx_lock);
+}
